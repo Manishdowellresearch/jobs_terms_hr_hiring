b6ceb7d062938880b04dd24367b1db3e5f590420
Manishdowellresearch <72200115+Manishdowellresearch@users.noreply.github.com>
2022-08-01 10:44:28 +0530

    Update GT.py

---
+++ Python/GT.py
@@ -0,0 +1,12 @@
+{"1":"You should have Github account. We will invite you to our github account then you have to take a branch of github main repository and do coding online and commit to branch.",
+"2":"Refer logic file, flow chart and prototype design to understand it before start coding.",
+"3":"Ask for your rights in database, platform and programmer ID to design team.",
+"4":"Any change in the design/task list you suggest is to be get approved from design team before coding.",
+"5":"Each task is to be entered to coding sheet as notes, then add your comments also.",
+"6":"When you complete coding our quality team will review the code and merge github branch to master.",
+"7":"We will provide platform for deployment, you have to implement the code according to our satisfaction.",
+"8":"You have to attend daily meeting with uxlivinglab team to update progress of work.",
+"9":"We will clarify all doubts while execution through Github, you can raise it as issue.",
+"10":"Work online in our platform provided for easy monitoring the task completion. Working in any other platform or Offline work is not permitted.",
+"11":"Ownership of the data/source code you entered is vested with dowell uxlivinglab and you have no rights and not supposed to share or use this for any other projects, read github licence for more details.",
+"12":"Do not reshare or subcontract this task to any others."}
