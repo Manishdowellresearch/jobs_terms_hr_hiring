91d529fa50bb5451047b3d31c9d2c518f588174b
Manishdowellresearch <72200115+Manishdowellresearch@users.noreply.github.com>
2022-08-01 10:48:22 +0530

    Update Paymet.py

---
+++ Python/Paymet.py
@@ -0,0 +1,5 @@
+{"1":"We will pay only through freelancer platform you are connected with DoWell uxlivinglab. Other payment modes are not acceptable.",
+"2":"Each task is designed for 20 hours in a week, you can work according to your convenience.",
+"3":"We will release payment every Saturday based on work you have submitted in that week. Rehire again on Monday if we are interested to continue with you. Rehire is our sole discretion.",
+"4":"Budget per week is 30 to 50USD. this includes all your expenses including payment to platform you have registered.",
+"5":"We have rights to ask for refund or damages if you are not completing the task according to our satisfaction or delaying the task."}
