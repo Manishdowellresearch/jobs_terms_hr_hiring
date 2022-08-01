c1fff336cc39374a858bb51ae04fb8cc8354a664
Manishdowellresearch <72200115+Manishdowellresearch@users.noreply.github.com>
2022-08-01 10:50:27 +0530

    Update workflow.py

---
+++ Python/workflow.py
@@ -0,0 +1,7 @@
+{"1:"Attend a initial meeting to ask queries you have. link for meeting - https://discord.gg/pqCP2wrgyy.",
+"2":"Order confirmation in the platform you are registered.",
+"3":"Connecting to team lead for daily meeting.",
+"4":"Execute the task - DESIGN > CODING > TESTING > DEPLOYMENT.",
+"5":"Submit work completion in your platform and request payment.",
+"6":"uxlivinglab approve your work for payment based on quality assessment.",
+"7":"Rehire for next week based on mutual acceptance."}
